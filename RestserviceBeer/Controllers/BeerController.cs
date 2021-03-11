57e88804766207a63cfc0e649c88da94a6780188
AndreasData <60772893+AndreasData@users.noreply.github.com>
2021-03-11 11:41:21 +0100

    Add project files.

---
+++ RestserviceBeer/Controllers/BeerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Beerlibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestserviceBeer.Manager;
+
+namespace RestserviceBeer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private BeerManager manager = new BeerManager();
+        // GET: api/Beer
+        [HttpGet]
+        public IEnumerable<Beer> Get()
+        {
+            return manager.GetAll();
+        }
+
+        // GET: api/Beer/5
+        [HttpGet("{id}")]
+        public Beer Get(int Id)
+        {
+            return manager.GetByID(Id);
+        }
+
+        // POST: api/Beer
+        [HttpPost]
+        public Beer Post([FromBody] Beer value)
+        {
+            return manager.Add(value);
+        }
+
+        // PUT: api/Beer/5
+        [HttpPut("{Id}")]
+        public Beer Put(int Id, [FromBody] Beer value)
+        {
+            return manager.Update(Id, value);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public Beer Delete(int Id)
+        {
+            return manager.Delete(Id);
+        }
+    }
+}
---
+++ RestserviceBeer/Manager/BeerManager.cs
@@ -0,0 +1,54 @@
+﻿using Beerlibrary;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace RestserviceBeer.Manager
+{
+    public class BeerManager
+    {
+        private static int _NextID = 1;
+        private static readonly List<Beer> Data = new List<Beer>
+        {
+            new Beer {Id = _NextID++, Name = "Tuborg", Abv = 8, Pris =37},
+            new Beer {Id = _NextID++, Name = "Carlsberg", Abv = 5, Pris =27}
+        };
+
+        public IEnumerable<Beer> GetAll()
+        {
+            return new List<Beer>(Data);
+        }
+
+        public Beer GetByID(int id)
+        {
+            return Data.Find(Beer => Beer.Id == id);
+        }
+
+        public Beer Add(Beer newBeer)
+        {
+            newBeer.Id = _NextID++;
+            Data.Add(newBeer);
+            return newBeer;
+        }
+
+        public Beer Update(int id, Beer updates)
+        {
+            Beer beer = Data.Find(book1 => book1.Id == id);
+            if (beer == null) return null;
+            beer.Name = updates.Name;
+            beer.Pris = updates.Pris;
+                return beer;
+        }
+
+        public Beer Delete(int Id)
+        {
+            Beer beer = Data.Find(book1 => book1.Id == Id);
+            if (beer == null) return null;
+            Data.Remove(beer);
+            return beer;
+        }
+    }
+}
